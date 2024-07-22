912dbc6cc96b5abec71e078d215707d5598cdf56
zkud <anton.kudyrka@gmail.com>
2024-07-22 07:36:56 +0000

    refact: reorganized macros/ast-term-derive

---
+++ macros/ast-term-derive/src/getters_setters.rs
@@ -0,0 +1,142 @@
+use proc_macro2::TokenStream as QuoteTokenStream;
+use quote::{format_ident, quote};
+use syn::{DeriveInput, Field, Fields};
+
+use super::util::type2str;
+
+pub fn impl_getters_and_setters_for_fields(input: &DeriveInput, fields: &Fields) -> QuoteTokenStream {
+  let mut tokens = quote! {};
+  for field in fields.iter() {
+    let field_tokens = impl_getters_and_setters_for_field(input, field);
+    tokens.extend(field_tokens);
+  }
+  tokens
+}
+
+fn impl_getters_and_setters_for_field(input: &DeriveInput, field: &Field) -> QuoteTokenStream {
+  if !getters_setters_are_required(field) {
+    return quote! {};
+  }
+  let mut tokens = quote! {};
+  let getter = impl_getter(input, field);
+  tokens.extend(getter);
+  let getter = impl_mut_getter(input, field);
+  tokens.extend(getter);
+  let setter = impl_setter(input, field);
+  tokens.extend(setter);
+  tokens
+}
+
+fn getters_setters_are_required(field: &Field) -> bool {
+  matches!(
+    field
+      .attrs
+      .iter()
+      .find(|attr| attr.path.is_ident("prop") || attr.path.is_ident("subnode_prop")),
+    Some(_)
+  )
+}
+
+fn impl_getter(input: &DeriveInput, field: &Field) -> QuoteTokenStream {
+  if let Some(field_name) = &field.ident {
+    let name = &input.ident;
+    let field_type = &field.ty;
+    let header = format!(
+      " Returns a reference to the {} field of the {}.",
+      field_name, name
+    );
+    let self_arg = format!(" * `&self` - A reference to the {}", name);
+    let returns = format!(
+      " * `&{}` - A reference to the {} field of the struct",
+      type2str(field_type),
+      field_name
+    );
+    quote! {
+      impl #name {
+        #[doc=#header]
+        #[doc=""]
+        #[doc=" # Arguments"]
+        #[doc=""]
+        #[doc=#self_arg]
+        #[doc=""]
+        #[doc=" # Returns"]
+        #[doc=""]
+        #[doc=#returns]
+        #[cfg(not(tarpaulin_include))]
+        pub fn #field_name(&self) -> &#field_type {
+          &self.#field_name
+        }
+      }
+    }
+  } else {
+    quote! {}
+  }
+}
+
+fn impl_mut_getter(input: &DeriveInput, field: &Field) -> QuoteTokenStream {
+  if let Some(field_name) = &field.ident {
+    let method_name = format_ident!("{}_mut", field_name);
+    let name = &input.ident;
+    let field_type = &field.ty;
+    let header = format!(
+      " Returns a mutable reference to the {} field of the {}.",
+      field_name, name
+    );
+    let self_arg = format!(" * `&mut self` - A mutable reference to the {}", name);
+    let returns = format!(
+      " * `&mut {}` - A mutable reference to the {} field of the struct",
+      type2str(field_type),
+      field_name
+    );
+    quote! {
+      impl #name {
+        #[doc=#header]
+        #[doc=""]
+        #[doc=" # Arguments"]
+        #[doc=""]
+        #[doc=#self_arg]
+        #[doc=""]
+        #[doc=" # Returns"]
+        #[doc=""]
+        #[doc=#returns]
+        #[cfg(not(tarpaulin_include))]
+        pub fn #method_name(&mut self) -> &mut #field_type {
+          &mut self.#field_name
+        }
+      }
+    }
+  } else {
+    quote! {}
+  }
+}
+
+fn impl_setter(input: &DeriveInput, field: &Field) -> QuoteTokenStream {
+  if let Some(field_name) = &field.ident {
+    let method_name = format_ident!("set_{}", field_name);
+    let name = &input.ident;
+    let field_type = &field.ty;
+    let header = format!(" Sets the {} field of the {}.", field_name, name);
+    let self_arg = format!(" * `&mut self` - A mutable reference to the {}", name);
+    let value_arg = format!(
+      " * `{}` - A new value for the {} field",
+      type2str(field_type),
+      field_name
+    );
+    quote! {
+      impl #name {
+        #[doc=#header]
+        #[doc=""]
+        #[doc=" # Arguments"]
+        #[doc=""]
+        #[doc=#self_arg]
+        #[doc=#value_arg]
+        #[cfg(not(tarpaulin_include))]
+        pub fn #method_name(&mut self, value: #field_type) {
+          self.#field_name = value;
+        }
+      }
+    }
+  } else {
+    quote! {}
+  }
+}
---
+++ macros/ast-term-derive/src/new.rs
@@ -0,0 +1,100 @@
+use proc_macro2::TokenStream as QuoteTokenStream;
+use quote::quote;
+use syn::{DeriveInput, Field, Fields};
+
+use super::util::type2str;
+
+pub fn impl_default_new_methods(
+  input: &DeriveInput,
+  fields: &Fields,
+) -> QuoteTokenStream {
+  let name = &input.ident;
+  let header = format!(" Creates a new instance of the {}.", name);
+  let doc_params = get_doc_params(fields);
+  let returns = format!(" * `{}` - A new instance of the {}", name, name);
+  let params = get_new_params(fields);
+  let struct_fields = get_new_struct_params(fields);
+  quote! {
+    impl #name {
+      #[doc=#header]
+      #[doc=""]
+      #[doc=" # Arguments"]
+      #[doc=""]
+      #doc_params
+      #[doc=""]
+      #[doc=" # Returns"]
+      #[doc=""]
+      #[doc=#returns]
+      #[cfg(not(tarpaulin_include))]
+      pub fn new(#params) -> #name {
+        #name {
+          #struct_fields
+        }
+      }
+    }
+  }
+}
+
+fn get_doc_params(fields: &Fields) -> QuoteTokenStream {
+  let mut tokens = quote! {};
+  for field in fields.iter() {
+    let field_tokens = get_doc_param_def(field);
+    tokens.extend(field_tokens);
+  }
+  tokens
+}
+
+fn get_doc_param_def(field: &Field) -> QuoteTokenStream {
+  if let Some(field_name) = &field.ident {
+    let field_type = &field.ty;
+    let value_arg = format!(
+      " * `{}` - A value for the {} field",
+      type2str(field_type),
+      field_name
+    );
+    quote! {
+      #[doc=#value_arg]
+    }
+  } else {
+    quote! {}
+  }
+}
+
+fn get_new_params(fields: &Fields) -> QuoteTokenStream {
+  let mut tokens = quote! {};
+  for field in fields.iter() {
+    let field_tokens = get_param_def(field);
+    tokens.extend(field_tokens);
+  }
+  tokens
+}
+
+fn get_param_def(field: &Field) -> QuoteTokenStream {
+  if let Some(field_name) = &field.ident {
+    let field_type = &field.ty;
+    quote! {
+      #field_name: #field_type,
+    }
+  } else {
+    quote! {}
+  }
+}
+
+fn get_new_struct_params(fields: &Fields) -> QuoteTokenStream {
+  let mut tokens = quote! {};
+  for field in fields.iter() {
+    let field_tokens = get_struct_param_def(field);
+    tokens.extend(field_tokens);
+  }
+  tokens
+}
+
+fn get_struct_param_def(field: &Field) -> QuoteTokenStream {
+  if let Some(field_name) = &field.ident {
+    quote! {
+      #field_name,
+    }
+  } else {
+    quote! {}
+  }
+}
