d460a0e410f04e7815ff1daca4d6b3f20ced32eb
zkud <anton.kudyrka@gmail.com>
2024-12-09 11:31:21 +0000

    test: location coverage

--- src/ast/common/location.rs
+++ src/ast/common/location.rs
@@ -29,3 +29,55 @@ impl Location {
         &self.filepath
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::Path;
+
+    #[test]
+    fn test_location_creation() {
+        let filepath = Path::new("test/file/path.rs");
+        let location = Location::new(10, 20, &filepath);
+
+        assert_eq!(location.start(), 10);
+        assert_eq!(location.end(), 20);
+        assert_eq!(&location.filepath(), &filepath);
+    }
+
+    #[test]
+    fn test_location_equality() {
+        let filepath1 = Path::new("test/file/path.rs");
+        let filepath2 = Path::new("test/file/path.rs");
+        let location1 = Location::new(10, 20, &filepath1);
+        let location2 = Location::new(10, 20, &filepath2);
+
+        assert_eq!(location1, location2);
+    }
+
+    #[test]
+    fn test_location_clone() {
+        let filepath = Path::new("test/file/path.rs");
+        let location = Location::new(10, 20, &filepath);
+        let cloned_location = location.clone();
+
+        assert_eq!(location, cloned_location);
+    }
+
+    #[test]
+    fn test_location_start_end() {
+        let filepath = Path::new("test/file/path.rs");
+        let location = Location::new(5, 15, &filepath);
+
+        assert_eq!(location.start(), 5);
+        assert_eq!(location.end(), 15);
+    }
+
+    #[test]
+    fn test_location_filepath() {
+        let filepath = Path::new("another/test/path.rs");
+        let location = Location::new(0, 0, &filepath);
+
+        assert_eq!(&location.filepath(), &filepath);
+    }
+}
