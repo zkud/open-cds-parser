0ad14cc9c2909dd4a389d5ecc3887c9f017e9da3
Anton Kudyrka <33183682+zkud@users.noreply.github.com>
2025-02-02 22:21:14 +0000

    feat: added "(" and ")" to ActionTerm

--- src/ast/cdl/action_term.rs
+++ src/ast/cdl/action_term.rs
@@ -1,6 +1,8 @@
 use super::name_term::NameTerm;
 use super::param_term::ParamTerm;
 use super::returns_term::ReturnsTerm;
+use super::super::OpenRoundBracketTerm;
+use super::super::CloseRoundBracketTerm;
 use ast_term_derive::ASTTerm;
 
 #[derive(ASTTerm, PartialEq, Eq, Debug, Clone)]
@@ -8,7 +10,11 @@ pub struct ActionTerm {
     #[subnode_prop]
     name: Box<NameTerm>,
     #[subnode_prop]
+    open_bracket: Box<OpenRoundBracketTerm>,
+    #[subnode_prop]
     params: Vec<ParamTerm>,
+    #[subnode_prop]
+    close_bracket: Box<CloseRoundBracketTerm>,
     #[subnode_prop]
     returned_type: Option<Box<ReturnsTerm>>,
 }
