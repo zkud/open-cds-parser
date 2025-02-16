d72104a89bc1a0482be9eec6ecf595bf596db0c2
Anton Kudyrka <33183682+zkud@users.noreply.github.com>
2025-02-16 18:15:44 +0000

    refact: mod rename

--- src/ast/cdl/actions_and_functions/mod.rs
+++ src/ast/cdl/actions_and_functions/mod.rs
@@ -2,10 +2,10 @@ mod action_declaration_term;
 mod action_term;
 mod function_term;
 mod param_term;
-mod returns_term;
+mod returns_declaration_term;
 
 pub use action_declaration_term::*;
 pub use action_term::*;
 pub use function_term::*;
 pub use param_term::*;
-pub use returns_term::*;
+pub use returns_declaration_term::*;
