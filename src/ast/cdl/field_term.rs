5161fa97755c8982b90648288a657d009d67dff8
zkud <anton.kudyrka@gmail.com>
2025-03-09 14:06:37 +0000

    feat: added locations where missing

--- src/ast/cdl/field_term.rs
+++ src/ast/cdl/field_term.rs
@@ -3,6 +3,8 @@ use ast_term_derive::ASTTerm;
 
 #[derive(ASTTerm, PartialEq, Eq, Debug, Clone)]
 pub struct FieldTerm {
+    #[prop]
+    location: Location,
     #[subnode_prop]
     name: Box<IdentifierTerm>,
     #[subnode_prop]
--- src/ast/cdl/path_term.rs
+++ src/ast/cdl/path_term.rs
@@ -5,5 +5,7 @@ use ast_term_derive::ASTTerm;
 #[derive(ASTTerm, PartialEq, Eq, Debug, Clone)]
 pub struct PathTerm {
     #[prop]
+    location: Location,
+    #[prop]
     value: PathBuf,
 }
