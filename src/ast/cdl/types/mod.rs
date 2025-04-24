aed925cefd706a72437857fa9aa81fe3a005dd69
zkud <anton.kudyrka@gmail.com>
2025-04-24 22:36:24 +0000

    feat: add array-like keyword terms to ast

--- src/ast/cdl/types/mod.rs
+++ src/ast/cdl/types/mod.rs
@@ -1,8 +1,14 @@
+mod array_term;
+mod many_term;
+mod of_term;
 mod simple_type_details_term;
 mod structured_type_details_term;
 mod type_declaration_term;
 mod type_reference_term;
 
+pub use array_term::*;
+pub use many_term::*;
+pub use of_term::*;
 pub use simple_type_details_term::*;
 pub use structured_type_details_term::*;
 pub use type_declaration_term::*;
---
+++ src/ast/cdl/types/of_term.rs
@@ -0,0 +1,8 @@
+use super::super::super::Location;
+use ast_term_derive::ASTTerm;
+
+#[derive(ASTTerm, PartialEq, Eq, Debug, Clone)]
+pub struct OfTerm {
+    #[prop]
+    location: Location,
+}
