db37090028a39e6c04dd2ded52125a4f8b5d3fd2
zkud <anton.kudyrka@gmail.com>
2025-04-27 17:52:49 +0000

    refact: added new terms which should drastically decrease codebase for keywords and punctuation signs

---
+++ src/ast/basic/keyword_term.rs
@@ -0,0 +1,24 @@
+use ast_term_derive::ASTTerm;
+
+/// Aggregate term for all keywords, like function, action, entity an so on
+#[derive(ASTTerm, PartialEq, Eq, Debug, Clone)]
+pub struct KeywordTerm {
+    #[prop]
+    location: Location,
+    #[prop]
+    keyword: Keyword,
+}
+
+/// All possible CDS keywords
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub enum Keyword {
+    As,       // as
+    Using,    // using
+    Action,   // action
+    Returns,  // returns
+    Function, // function
+    Array,    // array
+    Many,     // many
+    Of,       // of
+    From,     // from
+}
