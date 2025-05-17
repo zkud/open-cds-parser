26f1ca4bd23d8ea603efc68836e8c4617e1ec6f1
zkud <anton.kudyrka@gmail.com>
2025-05-17 18:47:33 +0000

    test: add unit tests for missing coverage

--- src/ast/cdl/types/array_prefix.rs
+++ src/ast/cdl/types/array_prefix.rs
@@ -22,3 +22,62 @@ impl Visitable for ArrayPrefix {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ast::basic::Keyword;
+    use crate::ast::common::{ASTTerm, Visitor};
+    use crate::ast::Location;
+
+    struct MockVisitor {
+        pub visits: Vec<Keyword>,
+    }
+
+    impl MockVisitor {
+        pub fn new() -> Self {
+            MockVisitor { visits: vec![] }
+        }
+    }
+
+    impl Visitor for MockVisitor {
+        type Error = ();
+
+        fn process<T: ASTTerm>(&mut self, term: &T) -> Result<(), Self::Error> {
+            if let Some(term) = term.try_convert::<KeywordTerm>() {
+                self.visits.push(term.keyword().clone());
+            }
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn with_array_of_it_visits_both_keywords() {
+        let location = Location::new_mock();
+        let array_term = KeywordTerm::new(location.clone(), Keyword::Array);
+        let of_term = KeywordTerm::new(location.clone(), Keyword::Of);
+        let prefix = ArrayPrefix::ArrayOf {
+            array: Box::new(array_term),
+            of: Box::new(of_term),
+        };
+        let mut visitor = MockVisitor::new();
+
+        let result = prefix.accept(&mut visitor);
+
+        assert!(result.is_ok());
+        assert_eq!(visitor.visits, vec![Keyword::Array, Keyword::Of]);
+    }
+
+    #[test]
+    fn with_many_it_visits_keyword() {
+        let location = Location::new_mock();
+        let many_term = KeywordTerm::new(location.clone(), Keyword::Many);
+        let prefix = ArrayPrefix::Many(Box::new(many_term));
+        let mut visitor = MockVisitor::new();
+
+        let result = prefix.accept(&mut visitor);
+
+        assert!(result.is_ok());
+        assert_eq!(visitor.visits, vec![Keyword::Many]);
+    }
+}
