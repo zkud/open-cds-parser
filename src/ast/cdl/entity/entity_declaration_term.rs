bbaf9d37f4c3f79ddc7d74fd7569dd420003ff9b
zkud <anton.kudyrka@gmail.com>
2025-05-24 05:52:26 +0000

    feat: add define, colon, semicolumn to entity declaration

--- src/ast/cdl/entity/entity_declaration_term.rs
+++ src/ast/cdl/entity/entity_declaration_term.rs
@@ -1,6 +1,7 @@
 use crate::ast::IdentifierTerm;
 use crate::ast::KeywordTerm;
 use crate::ast::Location;
+use crate::ast::PunctuationSignTerm;
 use crate::ast::StructureTerm;
 use ast_term_derive::ASTTerm;
 
@@ -9,11 +10,17 @@ pub struct EntityDeclarationTerm {
     #[prop]
     location: Location,
     #[subnode_prop]
+    define: Option<Box<KeywordTerm>>,
+    #[subnode_prop]
     entity: Box<KeywordTerm>,
     #[subnode_prop]
     identifier: Box<IdentifierTerm>,
     #[subnode_prop]
+    colon: Option<Box<PunctuationSignTerm>>,
+    #[subnode_prop]
     applied_aspects: Vec<IdentifierTerm>,
     #[subnode_prop]
     structure: Box<StructureTerm>,
+    #[subnode_prop]
+    semicolumn: Option<Box<PunctuationSignTerm>>,
 }
