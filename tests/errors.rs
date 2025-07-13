a07401c190eb0cf7c8d44dd8a13215cf74eadaf7
zkud <anton.kudyrka@gmail.com>
2025-07-13 17:36:09 +0000

    test: add integration test for linking errors

--- tests/errors.rs
+++ tests/errors.rs
@@ -1,5 +1,6 @@
 use open_cds_parser::ast::*;
 use open_cds_parser::parser::*;
+use std::collections::HashMap;
 use std::path::PathBuf;
 
 #[inline]
@@ -12,6 +13,11 @@ fn parse_module(path: &str) -> Result<Box<ModuleTerm>, ParseError> {
     setup_parser().parse_single_module(&PathBuf::from(path))
 }
 
+#[inline]
+fn parse_multiple_modules(path: &str) -> Result<HashMap<PathBuf, ModuleTerm>, ParseError> {
+    setup_parser().parse_multiple_modules(vec![PathBuf::from(path)])
+}
+
 #[test]
 fn with_syntax_errors_it_returns_a_readable_error() {
     let result = parse_module("./tests/projects/errors/srv/syntaxerror.cds");
@@ -22,4 +28,12 @@ Please see the internal parser error:";
         .unwrap_err()
         .to_string()
         .starts_with(expected_error_message));
+}
+
+#[test]
+fn with_linking_errors_it_returns_a_readable_error() {
+    let result = parse_multiple_modules("./tests/projects/errors/srv/linkingerror.cds");
+
+    let expected_error_message="Error(LinkingError): Cannot find the import './tests/projects/errors/srv/./missingpath' from ./tests/projects/errors/srv/linkingerror.cds";
+    assert_eq!(result.unwrap_err().to_string(), expected_error_message);
 }
---
+++ tests/projects/errors/srv/linkingerror.cds
@@ -0,0 +1,1 @@
+using * from './missingpath';
